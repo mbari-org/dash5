35aa5b04b062ef907dbad167c1ec5738656e5eff
Zack Lyon <zack.j.lyon@gmail.com>
2022-03-30 12:55:39 -0700

    Created accordion header component

--- packages/react-ui/src/Navigation/AccordionHeader.stories.tsx
+++ packages/react-ui/src/Navigation/AccordionHeader.stories.tsx
@@ -9,11 +9,17 @@ export default {
 } as Meta
 
 const Template: Story<AccordionHeaderProps> = (args) => (
-  <AccordionHeader {...args} />
+  <div className="bg-stone-100 p-2">
+    <AccordionHeader {...args} />
+  </div>
 )
 
 const args: AccordionHeaderProps = {
   className: '',
+  label: 'Handoff / On call',
+  secondaryLabel: 'Tanner P.(you) / Brian K.',
+  onToggle: () => {},
+  open: true,
 }
 
 export const Standard = Template.bind({})
@@ -22,5 +28,32 @@ Standard.parameters = {
   design: {
     type: 'figma',
     url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=224%3A131',
+  },
+}
+
+export const WithExpand = Template.bind({})
+WithExpand.args = { ...args, onExpand: () => {} }
+WithExpand.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=224%3A131',
+  },
+}
+
+export const WithoutSecondary = Template.bind({})
+WithoutSecondary.args = { ...args, secondaryLabel: undefined }
+WithoutSecondary.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=224%3A131',
+  },
+}
+
+export const Closed = Template.bind({})
+Closed.args = { ...args, open: false }
+Closed.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=221%3A188',
   },
 }
--- packages/react-ui/src/Navigation/AccordionHeader.tsx
+++ packages/react-ui/src/Navigation/AccordionHeader.tsx
@@ -1,17 +1,68 @@
 import React from 'react'
-import clsx from "clsx";
+import clsx from 'clsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown, faExpandArrows } from '@fortawesome/pro-solid-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { swallow } from '@mbari/utils'
 
 export interface AccordionHeaderProps {
   className?: string
   style?: React.CSSProperties
+  label: string
+  secondaryLabel?: string
+  onExpand?: () => void
+  onToggle: (open: boolean) => void
+  open?: boolean
 }
 
-export const AccordionHeader: React.FC<AccordionHeaderProps> = ({className, ...props}) => {
+const styles = {
+  container: 'flex font-display',
+  open: 'bg-primary-600 text-white',
+  closed: 'bg-white text-stone-800',
+  secondary: 'my-auto ml-3 text-sm italic',
+  textButton: 'text-md flex flex-grow px-3 py-2',
+  expandButton: 'pl-6 pr-2',
+  chevronButton: 'pl-2 pr-4',
+}
+
+export const AccordionHeader: React.FC<AccordionHeaderProps> = ({
+  className,
+  style,
+  label,
+  secondaryLabel,
+  onExpand,
+  onToggle,
+  open,
+}) => {
+  const handleToggle = swallow(() => {
+    onToggle(!open)
+  })
+
   return (
-    <div className={clsx("", className)}>
-      AccordionHeader Component
+    <div
+      style={style}
+      className={clsx(
+        styles.container,
+        className,
+        open ? styles.open : styles.closed
+      )}
+    >
+      <button className={styles.textButton}>
+        <span className={clsx(open && 'font-semibold')}>{label}</span>{' '}
+        <span className={clsx(styles.secondary, !open && 'opacity-60')}>
+          {secondaryLabel}
+        </span>
+      </button>
+      {onExpand && (
+        <button className={styles.expandButton} onClick={swallow(onExpand)}>
+          <FontAwesomeIcon icon={faExpandArrows as IconProp} />
+        </button>
+      )}
+      <button className={styles.chevronButton} onClick={handleToggle}>
+        <FontAwesomeIcon icon={faChevronDown as IconProp} />
+      </button>
     </div>
   )
 }
 
-AccordionHeader.displayName = "Components.AccordionHeader";
+AccordionHeader.displayName = 'Components.AccordionHeader'
