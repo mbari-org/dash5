66d76df2d0101faf57e7cc2853b66801aa8a1c4e
Zack Lyon <zack.j.lyon@gmail.com>
2022-04-04 09:56:44 -0700

    Create ScheduleCell and related stories

--- packages/react-ui/src/Cells/ScheduleCell.stories.tsx
+++ packages/react-ui/src/Cells/ScheduleCell.stories.tsx
@@ -4,18 +4,30 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import { ScheduleCell, ScheduleCellProps } from './ScheduleCell'
 
 export default {
-  title: 'ScheduleCell',
+  title: 'Cells/ScheduleCell',
   component: ScheduleCell,
 } as Meta
 
-const Template: Story<ScheduleCellProps> = (args) => <ScheduleCell {...args} />
+const Template: Story<ScheduleCellProps> = (args) => (
+  <div className="bg-stone-200 p-2">
+    <ScheduleCell {...args} />
+  </div>
+)
 
 const args: ScheduleCellProps = {
   className: '',
+  status: 'scheduled',
+  label: 'ESP sample at depth',
+  secondary: 'Deeper sample',
+  name: 'Reiko Michisaki',
+  description: 'Today',
 }
 
 export const Scheduled = Template.bind({})
-Scheduled.args = args
+Scheduled.args = {
+  ...args,
+  description2: '23:00-00:59',
+}
 Scheduled.parameters = {
   design: {
     type: 'figma',
@@ -24,7 +36,15 @@ Scheduled.parameters = {
 }
 
 export const Running = Template.bind({})
-Running.args = args
+Running.args = {
+  ...args,
+  status: 'running',
+  label: 'Profile Station',
+  secondary: 'One more time',
+  description: 'Started at 14:30',
+  description2: 'Running for 12min',
+  description3: 'Ending ~22:59',
+}
 Running.parameters = {
   design: {
     type: 'figma',
@@ -33,7 +53,14 @@ Running.parameters = {
 }
 
 export const Ended = Template.bind({})
-Ended.args = args
+Ended.args = {
+  ...args,
+  status: 'ended',
+  secondary: 'Per YZ shallow sample',
+  description: 'Ended at 10:22',
+  description2: '3 hours ago',
+  description3: '2h 14m runtime',
+}
 Ended.parameters = {
   design: {
     type: 'figma',
@@ -42,7 +69,14 @@ Ended.parameters = {
 }
 
 export const Executed = Template.bind({})
-Executed.args = args
+Executed.args = {
+  ...args,
+  status: 'executed',
+  label: 'resume',
+  secondary: 'fixed it',
+  description: 'Executed at 11:32',
+  description2: '86 min ago',
+}
 Executed.parameters = {
   design: {
     type: 'figma',
@@ -51,7 +85,12 @@ Executed.parameters = {
 }
 
 export const Paused = Template.bind({})
-Paused.args = args
+Paused.args = {
+  ...args,
+  status: 'paused',
+  description: 'Paused',
+  description2: 'Est. runtime 1h 59m',
+}
 Paused.parameters = {
   design: {
     type: 'figma',
--- packages/react-ui/src/Cells/ScheduleCell.tsx
+++ packages/react-ui/src/Cells/ScheduleCell.tsx
@@ -1,17 +1,102 @@
 import React from 'react'
-import clsx from "clsx";
+import clsx from 'clsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faClock,
+  faEllipsisV,
+  faCheck,
+  faTimes,
+} from '@fortawesome/pro-regular-svg-icons'
+import { faSync } from '@fortawesome/pro-light-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
 export interface ScheduleCellProps {
   className?: string
   style?: React.CSSProperties
+  status: 'scheduled' | 'running' | 'ended' | 'executed' | 'paused'
+  label: string
+  secondary: string
+  name: string
+  description: string
+  description2?: string
+  description3?: string
 }
 
-export const ScheduleCell: React.FC<ScheduleCellProps> = ({className, ...props}) => {
+const styles = {
+  container: 'flex font-display',
+  icon: 'flex items-center px-4',
+  labelContainer: 'flex flex-grow flex-col p-2',
+  label: 'flex text-blue-700', // needs updated colors
+  infoText: 'text-stone-500 opacity-60',
+  descriptionContainer: 'flex flex-col justify-center pr-8 pl-4',
+  options: 'flex items-center px-4',
+  open: 'font-semibold',
+  closed: 'opacity-60',
+}
+
+const icons: { [key: string]: IconProp } = {
+  scheduled: faClock as IconProp,
+  running: faSync as IconProp,
+  ended: faTimes as IconProp,
+  executed: faCheck as IconProp,
+  paused: faClock as IconProp,
+}
+
+export const ScheduleCell: React.FC<ScheduleCellProps> = ({
+  className,
+  status,
+  label,
+  secondary,
+  name,
+  description,
+  description2,
+  description3,
+  ...props
+}) => {
+  const backgroundColor = (() => {
+    if (status === 'running') return 'bg-indigo-400' // needs updated colors
+    if (status === 'paused') return 'bg-red-600' // needs updated colors
+    return 'bg-white'
+  })()
+
+  const isOpen = (() => {
+    return status === 'scheduled' || status === 'running' || status === 'paused'
+  })()
+
+  const iconColor = (() => {
+    if (status === 'running') return 'black'
+    if (status === 'paused') return styles.label
+    return styles.infoText
+  })()
+
   return (
-    <div className={clsx("", className)}>
-      ScheduleCell Component
+    <div className={clsx(styles.container, className, backgroundColor)}>
+      <button className={styles.icon}>
+        <FontAwesomeIcon
+          icon={icons[status]}
+          title="clock icon"
+          className={clsx(iconColor, 'text-2xl')}
+        />
+      </button>
+      <div className={styles.labelContainer}>
+        <button
+          className={clsx(styles.label, isOpen ? styles.open : styles.closed)}
+        >
+          {label}
+        </button>
+        <div className={clsx(styles.infoText, 'italic')}>{secondary}</div>
+        <div className={styles.infoText}>{name}</div>
+      </div>
+      <div className={styles.descriptionContainer}>
+        <div className={styles.infoText}>{description}</div>
+        {description2 && <div className={styles.infoText}>{description2}</div>}
+        {description3 && <div className={styles.infoText}>{description3}</div>}
+      </div>
+      <button className={styles.options}>
+        <FontAwesomeIcon icon={faEllipsisV as IconProp} className="text-2xl" />
+      </button>
     </div>
   )
 }
 
-ScheduleCell.displayName = "Components.ScheduleCell";
+ScheduleCell.displayName = 'Components.ScheduleCell'
