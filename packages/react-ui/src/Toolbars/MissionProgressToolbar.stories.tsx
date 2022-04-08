dbf343875d1414690c1cc1cd146e589a5359ee62
Jim Jeffers <jim@sumocreations.com>
2022-04-07 20:31:02 -0700

    Added stories to illustrate various use cases of mission progress.

--- packages/react-ui/src/Toolbars/MissionProgressToolbar.stories.tsx
+++ packages/react-ui/src/Toolbars/MissionProgressToolbar.stories.tsx
@@ -12,22 +12,75 @@ export default {
 } as Meta
 
 const Template: Story<MissionProgressToolbarProps> = (args) => (
-  <MissionProgressToolbar {...args} />
+  <p className="bg-secondary-100">
+    <MissionProgressToolbar {...args} />
+  </p>
 )
 
 const args: MissionProgressToolbarProps = {
   className: '',
-  height: 20,
-  width: 1024,
   ariaLabel: 'Mission Progress Toolbar',
   ticks: 5,
   startTime: DateTime.local().minus({ days: 3, hours: 7 }).toISO(),
-  endTime: DateTime.local().plus({ days: 2, hours: 3 }).toISO(),
+  endTime: DateTime.local().plus({ days: 1, hours: 3 }).toISO(),
 }
 
 export const Standard = Template.bind({})
 Standard.args = args
 Standard.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=1%3A599',
+  },
+}
+
+export const LotsOfTicks = Template.bind({})
+LotsOfTicks.args = { ...args, ticks: 10 }
+LotsOfTicks.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=1%3A599',
+  },
+}
+
+export const FewTicks = Template.bind({})
+FewTicks.args = { ...args, ticks: 2 }
+FewTicks.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=1%3A599',
+  },
+}
+
+export const NoTicks = Template.bind({})
+NoTicks.args = { ...args, ticks: 0 }
+NoTicks.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=1%3A599',
+  },
+}
+
+export const HasNotStarted = Template.bind({})
+HasNotStarted.args = {
+  ...args,
+  startTime: DateTime.local().plus({ days: 3, hours: 7 }).toISO(),
+  endTime: DateTime.local().plus({ days: 5, hours: 3 }).toISO(),
+}
+HasNotStarted.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=1%3A599',
+  },
+}
+
+export const HasEnded = Template.bind({})
+HasEnded.args = {
+  ...args,
+  startTime: DateTime.local().minus({ days: 5, hours: 7 }).toISO(),
+  endTime: DateTime.local().minus({ days: 3, hours: 3 }).toISO(),
+}
+HasEnded.parameters = {
   design: {
     type: 'figma',
     url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=1%3A599',
