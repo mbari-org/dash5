2d88b103e0e4cee4c83b319fbb867f68dc8db2f0
Zack Lyon <zack.j.lyon@gmail.com>
2022-04-11 08:38:02 -0700

    Change icon opacity and fix case on svg attributes

--- packages/react-ui/src/Cells/CommsCell.tsx
+++ packages/react-ui/src/Cells/CommsCell.tsx
@@ -23,7 +23,7 @@ const styles = {
   container: 'flex items-center bg-white p-4 font-display',
   detailsContainer: 'flex flex-grow flex-col pl-1',
   command: 'whitespace-pre-line font-light',
-  icon: 'px-6 text-2xl',
+  icon: 'px-6 text-2xl opacity-60',
   description: 'flex flex-grow flex-col p-2 opacity-60',
   buttonWrapper: 'flex w-full items-center text-left',
 }
@@ -40,14 +40,14 @@ const acknowledgeIcon = (
     <path
       d="M18.5 7.5L22.5 11.5L32.5 1.5"
       stroke="#6B7280"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M6.68717 27.8404H6.16355L5.86528 27.4101L2.67817 22.8116L2.28337 22.242L2.67817 21.6723L5.86528 17.0738L6.16355 16.6435H6.68717H9.9668V11.5V10.5H10.9668H14.0032H15.0032V11.5V16.6435H28.7213C30.3726 16.6435 31.7023 17.5319 32.5847 18.6069C33.4565 19.669 33.9978 21.0365 33.9978 22.242C33.9978 23.2791 33.7982 24.6585 33.012 25.8073C32.1826 27.0193 30.7911 27.8404 28.7213 27.8404H6.68717Z"
       stroke="#6B7280"
-      stroke-width="2"
+      strokeWidth="2"
     />
   </svg>
 )
@@ -65,12 +65,8 @@ export const CommsCell: React.FC<CommsCellProps> = ({
   isScheduled,
   onSelect,
 }) => {
-  const regFontEntry = (() => {
-    return entry.slice(0, -3)
-  })()
-  const boldFontEntry = (() => {
-    return entry.slice(-3)
-  })()
+  const regFontEntry = entry.slice(0, -3)
+  const boldFontEntry = entry.slice(-3)
   return (
     <article
       style={style}
