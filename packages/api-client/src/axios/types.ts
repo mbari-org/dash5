42428564dbfa8fb1f168a42ded40c448515dd646
Jim Jeffers <jim@sumocreations.com>
2022-04-11 14:37:04 -0700

    Request can be configured with typical axios headers.

--- packages/api-client/src/axios/types.ts
+++ packages/api-client/src/axios/types.ts
@@ -1,6 +1,7 @@
-import { AxiosInstance } from 'axios'
+import { AxiosInstance, AxiosRequestHeaders } from 'axios'
 
 export interface RequestConfig {
   debug?: boolean
   instance?: AxiosInstance
+  headers?: AxiosRequestHeaders
 }
