4fe89f9b99c46ac1760ee39bdeaf9b015611bbcf
Zack Lyon <zack.j.lyon@gmail.com>
2022-04-13 16:35:17 -0700

    Create layout and basic styling for overview toolbar

---
+++ packages/react-ui/src/Toolbars/OverviewToolbar.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { OverviewToolbar, OverviewToolbarProps } from './OverviewToolbar'
+import { faEye } from '@fortawesome/pro-light-svg-icons'
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+
+export default {
+  title: 'Toolbars/OverviewToolbar',
+  component: OverviewToolbar,
+} as Meta
+
+const Template: Story<OverviewToolbarProps> = (args) => (
+  <div className="bg-stone-200 p-2">
+    <OverviewToolbar {...args} />
+  </div>
+)
+
+const args: OverviewToolbarProps = {
+  className: '',
+  mission: 'Brizo 7 EcoHab',
+  btnLabel: 'Tanner P. (you)/ Brian K.',
+  btnIcon: faEye as IconDefinition,
+}
+
+export const Standard = Template.bind({})
+Standard.args = args
+Standard.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=1294%3A494',
+  },
+}
---
+++ packages/react-ui/src/Toolbars/OverviewToolbar.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { OverviewToolbar } from './OverviewToolbar'
+
+test.todo('should have tests')
+
+// test('should render child content', async () => {
+//   render(<OverviewToolbar>Click Here</OverviewToolbar>)
+//   expect(screen.getByText(/Click Here/i)).toHaveTextContent('Click Here')
+// })
