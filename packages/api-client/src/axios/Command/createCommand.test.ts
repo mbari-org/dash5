aebb4de6464db75bcb318ba5ca0012569f4edb0f
Jim Jeffers <jim@sumocreations.com>
2022-04-14 12:09:33 -0700

    Implemented createCommand request

---
+++ packages/api-client/src/axios/Command/createCommand.test.ts
@@ -0,0 +1,45 @@
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { createCommand, CreateCommandParams } from './createCommand'
+
+let params: CreateCommandParams = {
+  vehicle: 'example',
+  path: 'example',
+  commandText: 'example',
+  commandNote: 'example',
+  runCommand: 'example',
+  schedDate: 'example',
+}
+
+const mockResponse = { value: 'some-value' }
+const server = setupServer(
+  rest.post('/commands', (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockResponse))
+  })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+describe('createCommand', () => {
+  // TODO: Add tests for the actual API call
+  it('should return the mocked value when successful', async () => {
+    const response = await createCommand(params)
+    expect(response).toEqual(mockResponse)
+  })
+
+  it('should throw when unsuccessful', async () => {
+    server.use(
+      rest.post('/commands/preview', (_req, res, ctx) => {
+        return res.once(ctx.status(500))
+      })
+    )
+
+    try {
+      await createCommand(params)
+    } catch (error) {
+      expect(error).toBeDefined()
+    }
+  })
+})
