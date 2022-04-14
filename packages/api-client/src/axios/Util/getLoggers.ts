984500c73db010c9dbbf571295066900d6f5e5c7
Jim Jeffers <jim@sumocreations.com>
2022-04-14 11:06:22 -0700

    Implemented getLoggers request.

---
+++ packages/api-client/src/axios/Util/getLoggers.ts
@@ -0,0 +1,29 @@
+// Use scaffold axiosBase to generate the resources imported below.
+import { getInstance } from '../getInstance'
+import { RequestConfig } from '../types'
+
+export interface Logger {
+  id: string
+  name: string
+  level: string
+}
+
+export interface GetLoggersParams {}
+
+export interface GetLoggersResponse {
+  loggers: Logger[]
+}
+
+export const getLoggers = async (
+  params: GetLoggersParams,
+  { debug, instance = getInstance() }: RequestConfig = {}
+) => {
+  const url = '/util/logger'
+
+  if (debug) {
+    console.debug(`GET ${url}`)
+  }
+
+  const response = await instance.get(url, params)
+  return response.data as GetLoggersResponse
+}
