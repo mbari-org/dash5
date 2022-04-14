d4cb5a56372a2b887bd8a064c1b57e8ba5989071
Jim Jeffers <jim@sumocreations.com>
2022-04-14 11:06:30 -0700

    Implemented sendEmail request.

---
+++ packages/api-client/src/axios/Util/sendEmail.ts
@@ -0,0 +1,28 @@
+// Use scaffold axiosBase to generate the resources imported below.
+import { getInstance } from "../getInstance";
+import { RequestConfig } from "../types";
+
+export interface SendEmailParams {
+  email: string
+  subject: string
+  text: string
+  plainText: string 
+}
+
+export interface SendEmailResponse {
+  
+}
+
+export const sendEmail = async (
+  params: SendEmailParams,
+  { debug, instance = getInstance() }: RequestConfig = {}
+) => {
+  const url = "/util/email";
+
+  if (debug) {
+    console.debug(`POST ${url}`);
+  }
+
+  const response = await instance.post(url, params);
+  return response.data as SendEmailResponse;
+};
