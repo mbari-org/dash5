4818895e0a031e1fc7e364980bd93facb8e53c33
Jim Jeffers <jim@sumocreations.com>
2022-04-14 12:10:28 -0700

    Implemented updateDeployment request

---
+++ packages/api-client/src/axios/Deployment/updateDeployment.ts
@@ -0,0 +1,32 @@
+// Use scaffold axiosBase to generate the resources imported below.
+import { getInstance } from "../getInstance";
+import { RequestConfig } from "../types";
+
+export interface UpdateDeploymentParams {
+  startDate: string
+  launchDate: string
+  launchNote: string
+  recoverDate: string
+  recoverNote: string
+  endDate: string
+  name: string
+  tag: string 
+}
+
+export interface UpdateDeploymentResponse {
+  result: string
+}
+
+export const updateDeployment = async (
+  params: UpdateDeploymentParams,
+  { debug, instance = getInstance() }: RequestConfig = {}
+) => {
+  const url = "/deployments";
+
+  if (debug) {
+    console.debug(`PUT ${url}`);
+  }
+
+  const response = await instance.put(url, params);
+  return response.data as UpdateDeploymentResponse;
+};
