e4f91e3a87b37329d1e9e9d9d0bf6ea419c18432
Jim Jeffers <jim@sumocreations.com>
2022-04-14 11:06:42 -0700

    Implemented updateLogger request.

---
+++ packages/api-client/src/axios/Util/updateLogger.test.ts
@@ -0,0 +1,41 @@
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { updateLogger, UpdateLoggerParams } from './updateLogger'
+
+let params: UpdateLoggerParams = {
+  name: 'example',
+  level: 'example',
+}
+
+const mockResponse = { value: 'some-value' }
+const server = setupServer(
+  rest.put('/util/logger', (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockResponse))
+  })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+describe('updateLogger', () => {
+  // TODO: Add tests for the actual API call
+  it('should return the mocked value when successful', async () => {
+    const response = await updateLogger(params)
+    expect(response).toEqual(mockResponse)
+  })
+
+  it('should throw when unsuccessful', async () => {
+    server.use(
+      rest.put('/util/logger', (_req, res, ctx) => {
+        return res.once(ctx.status(500))
+      })
+    )
+
+    try {
+      await updateLogger(params)
+    } catch (error) {
+      expect(error).toBeDefined()
+    }
+  })
+})
---
+++ packages/api-client/src/axios/Util/updateLogger.ts
@@ -0,0 +1,27 @@
+// Use scaffold axiosBase to generate the resources imported below.
+import { getInstance } from '../getInstance'
+import { RequestConfig } from '../types'
+import { Logger } from './getLoggers'
+
+export interface UpdateLoggerParams {
+  name: string
+  level: string
+}
+
+export interface UpdateLoggerResponse {
+  logger: Logger
+}
+
+export const updateLogger = async (
+  params: UpdateLoggerParams,
+  { debug, instance = getInstance() }: RequestConfig = {}
+) => {
+  const url = '/util/logger'
+
+  if (debug) {
+    console.debug(`PUT ${url}`)
+  }
+
+  const response = await instance.put(url, params)
+  return response.data as UpdateLoggerResponse
+}
