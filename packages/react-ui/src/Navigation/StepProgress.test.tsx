3c5978c0f961bbf774e3600f5908ed31c3c710b0
Zack Lyon <zack.j.lyon@gmail.com>
2022-04-15 10:48:34 -0700

    Create step progress bar and basic styling

---
+++ packages/react-ui/src/Navigation/StepProgress.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { StepProgress } from './StepProgress'
+
+test.todo('tests go here')
+// test('should render child content', async () => {
+//   render(<StepProgress/>)
+//   expect(screen.getByText(/Click Here/i)).toHaveTextContent('Click Here')
+// })
---
+++ packages/react-ui/src/Navigation/StepProgress.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import clsx from 'clsx'
+
+export interface StepProgressProps {
+  className?: string
+  style?: React.CSSProperties
+  steps: Step[]
+}
+
+interface Step {
+  id: string
+  title: string
+  inProgress: boolean
+}
+
+const styles = {
+  container: 'bg-white p-2 font-display',
+  step: 'mr-2 flex flex-grow justify-center py-2 px-4 text-xl',
+  active: 'bg-primary-600 text-white',
+  inactive: 'bg-stone-200 text-stone-500 opacity-90',
+}
+
+export const StepProgress: React.FC<StepProgressProps> = ({
+  className,
+  steps,
+}) => {
+  return (
+    <article className={clsx(styles.container, className)}>
+      <ul className="flex">
+        {steps.map(({ title, inProgress }, index) => (
+          <li
+            key={index}
+            className={clsx(
+              styles.step,
+              inProgress ? styles.active : styles.inactive
+            )}
+          >
+            <span>{index + 1}</span>
+            {'.'}
+            <span className="pl-2">{title}</span>
+          </li>
+        ))}
+      </ul>
+    </article>
+  )
+}
+
+StepProgress.displayName = 'Components.StepProgress'
