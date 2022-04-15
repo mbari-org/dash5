b13b721546cb18632c41bab946a11a92e92ec16e
Zack Lyon <zack.j.lyon@gmail.com>
2022-04-15 11:13:28 -0700

    Add step progress bar tests

--- packages/react-ui/src/Navigation/StepProgress.test.tsx
+++ packages/react-ui/src/Navigation/StepProgress.test.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { StepProgress } from './StepProgress'
+import { StepProgress, StepProgressProps } from './StepProgress'
 
-test.todo('tests go here')
-// test('should render child content', async () => {
-//   render(<StepProgress/>)
-//   expect(screen.getByText(/Click Here/i)).toHaveTextContent('Click Here')
-// })
+const props: StepProgressProps = {
+  steps: [
+    { id: '1', title: 'Mission', inProgress: true },
+    { id: '2', title: 'Waypoints', inProgress: false },
+  ],
+}
+
+test('should render step title', async () => {
+  render(<StepProgress {...props} />)
+  expect(screen.getByText(props.steps[0].title)).toBeInTheDocument()
+})
+
+test('should display step with a blue background and white text when in progress', async () => {
+  props.steps[0].inProgress = true
+  render(<StepProgress {...props} />)
+
+  const step = screen.getByText(props.steps[0].title).closest('li')
+
+  expect(step).toHaveClass('bg-primary-600')
+  expect(step).toHaveClass('text-white')
+})
+
+test('should display step with a light gray background and dark gray text when in progress', async () => {
+  props.steps[1].inProgress = false
+  render(<StepProgress {...props} />)
+
+  const step = screen.getByText(props.steps[1].title).closest('li')
+
+  expect(step).toHaveClass('bg-stone-200')
+  expect(step).toHaveClass('text-stone-500')
+})
