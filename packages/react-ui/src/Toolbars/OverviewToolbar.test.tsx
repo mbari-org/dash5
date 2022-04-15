273a557ea16322665889fa68ef2abc23b4820d22
Zack Lyon <zack.j.lyon@gmail.com>
2022-04-14 17:18:31 -0700

    Add extra interactions and rename props for clarity

--- packages/react-ui/src/Toolbars/OverviewToolbar.test.tsx
+++ packages/react-ui/src/Toolbars/OverviewToolbar.test.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { OverviewToolbar } from './OverviewToolbar'
+import { OverviewToolbar, OverviewToolbarProps } from './OverviewToolbar'
+import { faEye } from '@fortawesome/pro-light-svg-icons'
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
 
-test.todo('should have tests')
+const props: OverviewToolbarProps = {
+  deployment: 'Brizo 7 EcoHab',
+  pilotInCharge: 'Tanner P. (you)',
+  pilotOnCall: 'Brian K.',
+  btnIcon: faEye as IconDefinition,
+  open: false,
+  onClickDeployment: () => {},
+  onClickMissions: () => {},
+  onClickPilot: () => {},
+  onIcon1hover: () => <></>,
+  onIcon2hover: () => <></>,
+}
 
-// test('should render child content', async () => {
-//   render(<OverviewToolbar>Click Here</OverviewToolbar>)
-//   expect(screen.getByText(/Click Here/i)).toHaveTextContent('Click Here')
-// })
+test('should render mission name to the screen', async () => {
+  render(<OverviewToolbar {...props} />)
+
+  expect(screen.getByText(props.deployment)).toBeInTheDocument()
+})
+
+test('should render button labels', async () => {
+  render(
+    <OverviewToolbar
+      {...props}
+      pilotInCharge={'first pilot'}
+      pilotOnCall={'second pilot'}
+    />
+  )
+
+  expect(screen.getByText(/first pilot/i)).toBeInTheDocument()
+  expect(screen.getByText(/second pilot/i)).toBeInTheDocument()
+})
