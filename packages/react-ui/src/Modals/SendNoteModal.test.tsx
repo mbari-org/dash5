8a5e26474661d772fa3d51122e4da8f5dc90b7cd
John Sawiris <johnedwardsawiris@gmail.com>
2022-04-24 19:05:18 -0400

    fix: typo in import.

--- packages/react-ui/src/Modals/SendNoteModal.test.tsx
+++ packages/react-ui/src/Modals/SendNoteModal.test.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { SendNoteModal, SendNoteModalProps } from './SendNoteModal'
-import { SendNoteValues } from '../Forms/SendNote'
+import { SendNoteFormValues } from '../Forms/SendNoteForm'
 import { wait } from '@mbari/utils'
 
 const props: SendNoteModalProps = {
   vehicleName: 'example',
   open: true,
-  onSubmit: async (values: SendNoteValues) => {
+  onSubmit: async (values: SendNoteFormValues) => {
     await wait(1)
     return undefined
   },
