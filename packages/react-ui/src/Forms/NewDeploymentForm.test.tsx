46f13021e7a467869d35904bd92d7b23d50f6cbd
John Sawiris <johnedwardsawiris@gmail.com>
2022-04-24 21:17:46 -0400

    test: added tests for new deployment form

--- packages/react-ui/src/Forms/NewDeploymentForm.test.tsx
+++ packages/react-ui/src/Forms/NewDeploymentForm.test.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import { DateTime } from 'luxon'
+
 import { NewDeploymentForm, NewDeploymentFormValues } from './NewDeploymentForm'
 
-const handleSubmit = async (values: NewDeploymentFormValues) => {
-  await new Promise((res) => setTimeout(res, 1000))
-  return undefined
+const defaultValues: NewDeploymentFormValues = {
+  deploymentName: 'Deployment Example',
+  gitTag: 'v1.4.5',
+  startTime: DateTime.local().toISO(),
 }
 
-test('should render the initial value', async () => {
-  render(<NewDeploymentForm defaultValues={{}} onSubmit={handleSubmit} />)
-  expect(screen.getByDisplayValue('admin@example.com')).toBeInTheDocument()
-  expect(screen.getByDisplayValue('123456789')).toBeInTheDocument()
+describe('NewDeploymentForm', () => {
+  test('should render the initial value', async () => {
+    const onSubmit = jest.fn()
+
+    render(
+      <NewDeploymentForm defaultValues={defaultValues} onSubmit={onSubmit} />
+    )
+
+    const input = screen.getByDisplayValue('Deployment Example')
+    const submitBtn = screen.getByText(/submit/i)
+
+    fireEvent.change(input, { target: { value: 'New Deployment' } })
+
+    fireEvent.click(submitBtn)
+
+    await waitFor(() => {
+      expect(onSubmit).toBeCalledTimes(1)
+
+      expect(onSubmit).toBeCalledWith({
+        ...defaultValues,
+        deploymentName: 'New Deployment',
+      })
+    })
+  })
 })
