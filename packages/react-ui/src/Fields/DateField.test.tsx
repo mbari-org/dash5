3e543068146c0f1dc63bcff2b7865386a3960343
Jim Jeffers <jim@sumocreations.com>
2022-04-26 10:15:39 -0700

    Remove unused values.

--- packages/react-ui/src/Fields/DateField.test.tsx
+++ packages/react-ui/src/Fields/DateField.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { DateField } from './DateField'
 
