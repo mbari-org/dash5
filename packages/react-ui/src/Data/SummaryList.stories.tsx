11bbf8f95faa98921a66080791e8ce5ff1cbfa81
Zack Lyon <zack.j.lyon@gmail.com>
2022-05-13 11:38:06 -0700

    Add SummaryList design reference to story

--- packages/react-ui/src/Data/SummaryList.stories.tsx
+++ packages/react-ui/src/Data/SummaryList.stories.tsx
@@ -13,12 +13,31 @@ const Template: Story<SummaryListProps> = (args) => <SummaryList {...args} />
 const args: SummaryListProps = {
   header: 'ESTIMATES',
   values: [
-    'Total mission time: 12 minutes of 6h 10m',
-    'Time in transit: 1.5 of 4 hours complete',
-    'Travel distance: 3.6 of 7.2km complete',
-    'Bottom depth: 100 to 180 meters',
+    <div key={1}>
+      <span className="mr-1 font-medium">Total mission time:</span>
+      <span> 6 hours 10 minutes</span>
+    </div>,
+    <div key={2}>
+      <span className="mr-1 font-medium">Time in transit:</span>
+      <span>4 hours</span>
+    </div>,
+    <div key={3}>
+      <span className="mr-1 font-medium">Travel distance:</span>
+      <span>7.2km</span>
+    </div>,
+    <div key={4}>
+      <span className="mr-1 font-medium">Bottom depth:</span>
+      <span>100 to 180 meters</span>
+    </div>,
   ],
 }
 
 export const Standard = Template.bind({})
 Standard.args = args
+
+Standard.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=1409%3A613',
+  },
+}
