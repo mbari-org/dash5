98f1f141b2a325282dd36a56a3b67d22c3ef36fc
Jim Jeffers <jim@sumocreations.com>
2022-06-10 14:11:35 -0700

    Implemented `CellVirtualizer` component.

---
+++ packages/react-ui/src/Cells/CellVirtualizer.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { CellVirtualizer, CellVirtualizerProps } from './CellVirtualizer'
+import { Running } from './VehicleCell.stories'
+import { VehicleCell, VehicleCellProps } from './VehicleCell'
+import { VehicleProps } from '../Diagrams'
+
+export default {
+  title: 'CellVirtualizer',
+  component: CellVirtualizer,
+} as Meta
+
+const Template: Story<CellVirtualizerProps> = (args) => (
+  <CellVirtualizer {...args} />
+)
+
+const args: CellVirtualizerProps = {
+  className: 'h-96 w-[438px]',
+  count: 100,
+  estimateSize: () => 100,
+  cellAtIndex: (index: number) => (
+    <div className="border-b border-stone-200">
+      <VehicleCell
+        {...(Running.args as VehicleCellProps)}
+        vehicle={{
+          ...(Running.args?.vehicle as VehicleProps),
+          textVehicle: `Vehicle ${index}`,
+        }}
+      />
+    </div>
+  ),
+}
+
+export const Standard = Template.bind({})
+Standard.args = args
