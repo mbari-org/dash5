69e9b57e67c2d572218c34d6556d03591e005c13
Jim Jeffers <jim@sumocreations.com>
2022-06-30 10:34:28 -0700

    Revised useCreateUser config variable.

--- packages/api-client/src/react-query/User/useCreateUser.ts
+++ packages/api-client/src/react-query/User/useCreateUser.ts
@@ -1,9 +1,14 @@
 import { useMutation } from 'react-query'
 import { CreateUserParams, createUser, RequestConfig } from '../../axios'
+import { useAuthContext } from '../AuthProvider'
 
-export const useCreateUser = (config: RequestConfig) => {
+export const useCreateUser = (config?: RequestConfig) => {
+  const { axiosInstance } = useAuthContext()
   const mutation = useMutation((params: CreateUserParams) => {
-    return createUser(params, config)
+    return createUser(params, {
+      ...(config ?? {}),
+      instance: config?.instance ?? axiosInstance,
+    })
   })
   return mutation
 }
