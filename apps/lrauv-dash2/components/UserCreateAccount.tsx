b8c3761956d4a4b68086787c66da2ad60361ce49
Jim Jeffers <jim@sumocreations.com>
2022-07-01 16:22:42 -0700

    Setup reCaptcha V2 on CreateUserModal.

--- apps/lrauv-dash2/components/UserCreateAccount.tsx
+++ apps/lrauv-dash2/components/UserCreateAccount.tsx
@@ -1,34 +1,63 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
-import { useAuthContext } from '@mbari/api-client'
+import { useCreateUser, useTethysApiContext } from '@mbari/api-client'
 import { AsyncSubmitHandler } from '@sumocreations/forms'
 import { CreateAccountModal, CreateAccountFormValues } from '@mbari/react-ui'
+import ReCAPTCHA from 'react-google-recaptcha'
 
 const UserCreateAccount: React.FC<{ onClose?: () => void }> = ({
   onClose: handleClose,
 }) => {
-  const { login, loading, error } = useAuthContext()
+  const { siteConfig } = useTethysApiContext()
+  const [recaptchaResponse, setRecaptchaResponse] =
+    useState<string | null>(null)
+  const {
+    mutate: createUser,
+    isLoading,
+    isError,
+    isSuccess,
+    error,
+    data,
+  } = useCreateUser({})
 
   const handleSubmit: AsyncSubmitHandler<CreateAccountFormValues> = async (
     values
   ) => {
-    await login(values.email, values.password)
+    await createUser({ ...values, recaptchaResponse })
     return undefined
   }
 
   useEffect(() => {
-    if (!loading && error) {
-      toast.error(error)
+    if (!isLoading && isError) {
+      toast.error(error?.message ?? 'Could not process your reset request.')
     }
-  }, [loading, error])
+  }, [isLoading, isError, error])
+
+  useEffect(() => {
+    if (!isLoading && isSuccess) {
+      toast.success(
+        data?.result.message ??
+          'A link to reset your password has been sent if an account with the specified email address exists.'
+      )
+      handleClose?.()
+    }
+  }, [isLoading, isSuccess, data, handleClose])
 
   return (
-    <CreateAccountModal
-      onSubmit={handleSubmit}
-      loading={loading}
-      onClose={handleClose}
-      open
-    />
+    <>
+      <CreateAccountModal
+        onSubmit={handleSubmit}
+        loading={isLoading}
+        onClose={handleClose}
+        open
+        disableConfirm={!recaptchaResponse}
+      >
+        <ReCAPTCHA
+          sitekey={siteConfig?.appConfig.recaptcha.siteKey}
+          onChange={setRecaptchaResponse}
+        />
+      </CreateAccountModal>
+    </>
   )
 }
 
