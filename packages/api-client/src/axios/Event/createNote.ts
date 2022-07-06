5092759142d826881b57a5a33f3f579f2de67bf3
Jim Jeffers <jim@sumocreations.com>
2022-07-06 12:11:08 -0700

    Updated createNote response / payload.

--- packages/api-client/src/axios/Event/createNote.ts
+++ packages/api-client/src/axios/Event/createNote.ts
@@ -1,14 +1,18 @@
 // Use scaffold axiosBase to generate the resources imported below.
 import { getInstance } from '../getInstance'
 import { RequestConfig } from '../types'
-
+import { filterBlankAttributes } from '@mbari/utils'
 export interface CreateNoteParams {
   vehicle: string
   note: string
+  bug?: boolean
 }
 
 export interface CreateNoteResponse {
-  result: string
+  date: string
+  status: string
+  vehicle: string
+  sentBy: string
 }
 
 export const createNote = async (
@@ -17,10 +21,18 @@ export const createNote = async (
 ) => {
   const url = '/events/note'
 
+  const completeUrl = `${url}?${new URLSearchParams(
+    filterBlankAttributes({
+      note: params.note,
+      vehicle: params.vehicle,
+      bug: params.bug ? 'y' : 'n',
+    })
+  )}`
+
   if (debug) {
-    console.debug(`POST ${url}`)
+    console.debug(`POST ${completeUrl}`)
   }
 
-  const response = await instance.post(url, params, config)
-  return response.data as CreateNoteResponse
+  const response = await instance.post(completeUrl, undefined, config)
+  return response.data.result as CreateNoteResponse
 }
