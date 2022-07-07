dae78de16bdc060bc434add5c0aa819fa511534f
Jim Jeffers <jim@sumocreations.com>
2022-07-07 12:37:21 -0700

    Added google fonts to NextJS project.

---
+++ apps/lrauv-dash2/pages/_document.tsx
@@ -0,0 +1,30 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />{' '}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;700&display=swap"
+          rel="stylesheet"
+        />{' '}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
