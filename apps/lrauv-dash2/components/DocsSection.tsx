82a6ac6d03e2df83ed59b8b3f40605d731f6982e
Jim Jeffers <jim@sumocreations.com>
2022-07-22 13:04:08 -0700

    DocsSection handles all filter type options.

--- apps/lrauv-dash2/components/DocsSection.tsx
+++ apps/lrauv-dash2/components/DocsSection.tsx
@@ -1,11 +1,69 @@
+import { useState } from 'react'
 import { AccordionCells, DocCell, SelectField } from '@mbari/react-ui'
-import { useDocuments } from '@mbari/api-client'
+import {
+  useDeployments,
+  useDocuments,
+  GetDocumentsResponse,
+} from '@mbari/api-client'
 import { DateTime } from 'luxon'
 import { faPlus } from '@fortawesome/pro-regular-svg-icons'
 import { AccessoryButton } from '@mbari/react-ui'
 
+type FilterType =
+  | 'This Vehicle'
+  | 'By Deployment'
+  | 'Unattached'
+  | 'Forms'
+  | 'Templates'
+  | 'All Documents'
+
+const FILTER_TYPES: FilterType[] = [
+  'This Vehicle',
+  'By Deployment',
+  'Unattached',
+  'Forms',
+  'Templates',
+  'All Documents',
+]
+
+const filterByType = (params: {
+  type: FilterType
+  doc: GetDocumentsResponse
+  deploymentId: string | null
+  vehicleName?: string
+}) => {
+  switch (params.type) {
+    case 'By Deployment':
+      if (params.deploymentId) {
+        const briefs = params.doc?.deploymentBriefs
+        const matches =
+          briefs?.filter(
+            (brief) => brief.deploymentId.toString() === params.deploymentId
+          ).length ?? 0
+        return matches > 0
+      }
+
+      return true
+    case 'This Vehicle':
+      return params.doc.vehicleNames?.includes(params.vehicleName ?? '')
+    case 'Unattached':
+      const hasBriefs = params.doc.deploymentBriefs?.length ?? 0
+      const hasVehicles = params.doc.vehicleNames?.length ?? 0
+      const isTemplate = params.doc.docType === 'TEMPLATE'
+      const isForm = params.doc.docType === 'FORM'
+      return !hasBriefs && !hasVehicles && !isTemplate && !isForm
+    case 'Forms':
+      return params.doc.docType === 'FORM'
+    case 'Templates':
+      return params.doc.docType === 'TEMPLATE'
+    default:
+      return true
+  }
+}
+
 interface DocsSectionProps {
   authenticated?: boolean
+  vehicleName: string
   onSelect?: () => void
   onSelectMore?: () => void
   onSelectMission?: (id: string) => void
@@ -16,8 +74,30 @@ const DocsSection: React.FC<DocsSectionProps> = ({
   onSelect,
   onSelectMission,
   onSelectMore,
+  vehicleName,
 }) => {
-  const { data } = useDocuments()
+  const [selectedType, setSelectedType] = useState<FilterType>('All Documents')
+  const [selectedDeployment, setSelectedDeployment] =
+    useState<null | string>(null)
+  const isFilteringDeployment = selectedType === 'By Deployment'
+
+  const { data: documentData } = useDocuments()
+  const { data: deploymentData } = useDeployments(
+    {
+      vehicle: vehicleName,
+    },
+    {
+      enabled: isFilteringDeployment,
+    }
+  )
+  const data = documentData?.filter((doc) =>
+    filterByType({
+      type: selectedType,
+      doc,
+      deploymentId: selectedDeployment,
+      vehicleName,
+    })
+  )
 
   const cellAtIndex = (index: number) => {
     const item = data?.[index]
@@ -54,11 +134,34 @@ const DocsSection: React.FC<DocsSectionProps> = ({
   return (
     <>
       <header className="flex p-2">
-        <SelectField name="Filter"></SelectField>
+        <div className="flex flex-grow flex-col">
+          <SelectField
+            name="Filter"
+            options={FILTER_TYPES.map((name) => ({ name, id: name }))}
+            className="flex-grow"
+            value={selectedType}
+            onSelect={(id) =>
+              setSelectedType((id ?? 'All Documents') as FilterType)
+            }
+          />
+          {isFilteringDeployment && (
+            <SelectField
+              name="Deployment"
+              options={deploymentData?.map((d) => ({
+                name: d.name,
+                id: d.deploymentId?.toString(),
+              }))}
+              className="flex-grow"
+              value={selectedDeployment ?? ''}
+              onSelect={(id) => setSelectedDeployment(id ?? null)}
+              clearable
+            />
+          )}
+        </div>
         {authenticated && (
-          <span className="flex flex-grow justify-end">
+          <div className="mb-auto flex pl-2">
             <AccessoryButton icon={faPlus} label={'Add Document'} />
-          </span>
+          </div>
         )}
       </header>
       <AccordionCells cellAtIndex={cellAtIndex} count={data?.length} />
