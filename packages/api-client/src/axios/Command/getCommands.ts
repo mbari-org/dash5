a43ea3c7dfef71c58d14e37d025e7d5c54c7d99f
Zack Lyon <zack.j.lyon@gmail.com>
2022-08-01 10:32:35 -0700

    Create getCommands request

---
+++ packages/api-client/src/axios/Command/getCommands.ts
@@ -0,0 +1,42 @@
+// Use scaffold axiosBase to generate the resources imported below.
+import { getInstance } from '../getInstance'
+import { RequestConfig } from '../types'
+
+export interface GetCommandsResponse {
+  commands: ApiCommand[]
+  serviceTypes: string[]
+  decimationTypes: string[]
+}
+
+interface ApiCommand {
+  advanced: boolean
+  description: string
+  keyword?: string
+  syntaxList: Syntax[]
+}
+
+interface Syntax {
+  argList: Arg[]
+  help: string
+}
+
+interface Arg {
+  argType: string
+  keyword?: string
+  required?: string
+}
+
+export const getCommands = async ({
+  debug,
+  instance = getInstance(),
+  ...config
+}: RequestConfig = {}) => {
+  const url = '/commands'
+
+  if (debug) {
+    console.debug(`GET ${url}`)
+  }
+
+  const response = await instance.get(url, config)
+  return response.data.result as GetCommandsResponse[]
+}
