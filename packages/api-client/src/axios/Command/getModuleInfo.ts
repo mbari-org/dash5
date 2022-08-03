e728fe05fac14994798c7e66970d9851c88c77f8
Zack Lyon <zack.j.lyon@gmail.com>
2022-08-03 10:40:13 -0700

    Create getModuleInfo request

---
+++ packages/api-client/src/axios/Command/getModuleInfo.ts
@@ -0,0 +1,42 @@
+// Use scaffold axiosBase to generate the resources imported below.
+import { getInstance } from '../getInstance'
+import { RequestConfig } from '../types'
+
+export interface GetModuleInfoResponse {
+  moduleNames: string[]
+  behaviors: Component
+  configUris: Module
+  outputUris: Module
+  sensors: Component
+  settingUris: Module
+  uris: Module
+  uriStringSettings: Module
+}
+
+interface Component {
+  [key: string]: Element[]
+}
+
+interface Module {
+  [key: string]: Component
+}
+
+interface Element {
+  string: string
+  description?: string
+}
+
+export const getModuleInfo = async ({
+  debug,
+  instance = getInstance(),
+  ...config
+}: RequestConfig = {}) => {
+  const url = '/commands/moduleInfo'
+
+  if (debug) {
+    console.debug(`GET ${url}`)
+  }
+
+  const response = await instance.get(url, config)
+  return response.data.result as GetModuleInfoResponse
+}
