37719258945c7b544bfb9d11d845540a8ab2fa7c
Zack Lyon <zack.j.lyon@gmail.com>
2022-08-08 17:22:56 -0700

    Create ConfirmVehicleDialog and stories

---
+++ packages/react-ui/src/Modals/ConfirmVehicleDialog.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0'
+import {
+  ConfirmVehicleDialog,
+  ConfirmVehicleDialogProps,
+} from './ConfirmVehicleDialog'
+
+export default {
+  title: 'Modals/ConfirmVehicleDialog',
+  component: ConfirmVehicleDialog,
+} as Meta
+
+const Template: Story<ConfirmVehicleDialogProps> = (args) => (
+  <ConfirmVehicleDialog {...args} />
+)
+
+const args: ConfirmVehicleDialogProps = {
+  vehicle: 'Brizo',
+  vehicleList: ['Brizo', 'Tethys', 'GupB'],
+  onCancel: () => console.log('cancel'),
+  onConfirm: () => console.log('confirm'),
+  onSubmit: (vehicle) => console.log(vehicle),
+}
+
+export const Command = Template.bind({})
+Command.args = { ...args, command: 'configSet' }
+
+Command.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=6863%3A989',
+  },
+}
+
+export const Mission = Template.bind({})
+Mission.args = { ...args, mission: 'sci2.xml' }
+
+Mission.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/FtsKsOCBQ2YjTZlwezG6aI/MBARI-Components?node-id=6865%3A1018',
+  },
+}
---
+++ packages/react-ui/src/Modals/ConfirmVehicleDialog.tsx
@@ -0,0 +1,103 @@
+import clsx from 'clsx'
+import React, { useState } from 'react'
+import { SelectField } from '../Fields'
+import { Dialog } from '../Modal/Dialog'
+import { FooterProps } from '../Modal/Footer'
+
+export interface ConfirmVehicleDialogProps extends FooterProps {
+  vehicle: string
+  vehicleList: string[]
+  mission?: string
+  command?: string
+  onSubmit?: (vehicle: string) => void
+}
+
+const styles = {
+  radio: 'checked:accent-teal-500 w-5 h-5 mr-2',
+  li: 'flex items-center',
+  select: 'ml-2 w-1/2',
+}
+
+export const ConfirmVehicleDialog: React.FC<ConfirmVehicleDialogProps> = ({
+  vehicle,
+  mission,
+  command,
+  vehicleList,
+  onConfirm,
+  onCancel,
+  onSubmit,
+}) => {
+  const [differentVehicle, setDifferentVehicle] =
+    useState<string | undefined>(undefined)
+
+  const [isCorrectVehicle, setIsCorrectVehicle] = useState(true)
+  const title = (
+    <div>
+      This {mission ? 'mission' : 'command'} should be scheduled for{' '}
+      <span className="text-teal-500">
+        {isCorrectVehicle ? vehicle : differentVehicle ?? vehicle}
+      </span>
+      . Is that right?
+    </div>
+  )
+
+  const vehicleOptions = vehicleList.map((name) => ({
+    id: name,
+    name,
+  }))
+
+  const handleConfirm = () => {
+    const confirmedVehicle = isCorrectVehicle ? vehicle : differentVehicle
+
+    onSubmit?.(confirmedVehicle ?? vehicle)
+  }
+
+  const message = (
+    <ul>
+      <li className={styles.li}>
+        <input
+          className={styles.radio}
+          type="radio"
+          name="vehicle"
+          id="default"
+          onChange={() => setIsCorrectVehicle(true)}
+          checked={isCorrectVehicle}
+        />
+        <label htmlFor="default">
+          Yes, {vehicle} should do {mission ? mission : command}
+        </label>
+      </li>
+      <li className={clsx(styles.li, 'mt-4')}>
+        <input
+          className={styles.radio}
+          type="radio"
+          name="vehicle"
+          id="selected"
+          onChange={() => setIsCorrectVehicle(false)}
+          checked={!isCorrectVehicle}
+        />
+        <label htmlFor="selected">No, this is for:</label>
+        <SelectField
+          name="vehicle list"
+          className={styles.select}
+          options={vehicleOptions}
+          placeholder="Select LRAUV"
+          value={differentVehicle}
+          onSelect={(id) => setDifferentVehicle(id ?? vehicle)}
+          required={!isCorrectVehicle}
+        />
+      </li>
+    </ul>
+  )
+
+  return (
+    <Dialog
+      open
+      title={title}
+      message={message}
+      onConfirm={onSubmit ? handleConfirm : onConfirm}
+      onCancel={onCancel}
+      disableConfirm={!isCorrectVehicle && !differentVehicle}
+    />
+  )
+}
