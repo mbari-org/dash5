fa2819e1c57d160a1420b705adb4eb3b8e5d38e8
Jim Jeffers <jim@sumocreations.com>
2023-01-19 09:30:40 -0800

    Increased playwright global timeout adjust circleCI config.

--- apps/lrauv-dash2/playwright.config.ts
+++ apps/lrauv-dash2/playwright.config.ts
@@ -10,7 +10,7 @@ const baseURL = `http://localhost:${PORT}`
 // Reference: https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
   // Timeout per test
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   // Test directory
   testDir: path.join(__dirname, 'e2e'),
   // If a test fails, retry it additional 2 times
