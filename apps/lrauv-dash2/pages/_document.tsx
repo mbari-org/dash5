eea6e13c602105dc120bef66b96f198ace25f46d
Karen A. Salamy <salamy@mbari.org>
2023-03-28 11:57:01 -0700

    Updated Favicon to LRAUV favicon image.

--- apps/lrauv-dash2/pages/_document.tsx
+++ apps/lrauv-dash2/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/lrauv-logo-w28.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
