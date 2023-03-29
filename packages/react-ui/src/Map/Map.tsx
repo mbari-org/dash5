f2c202c8ff4af632947391dd6a59c33350d43dd1
Karen A. Salamy <salamy@mbari.org>
2023-03-29 10:24:08 -0700

    Added maxNativeZoom property to Maps.tsx.

--- packages/react-ui/src/Map/Map.tsx
+++ packages/react-ui/src/Map/Map.tsx
@@ -15,6 +15,7 @@ export interface MapProps {
   zoom?: number
   minZoom?: number
   maxZoom?: number
+  maxNativeZoom?: number
   children?: React.ReactNode
 }
 
@@ -22,9 +23,10 @@ const Map: React.FC<MapProps> = ({
   className,
   style,
   center = [36.7849, -122.12097],
-  zoom = 11,
+  zoom = 17,
   minZoom = 4,
-  maxZoom = 16,
+  maxZoom = 17,
+  maxNativeZoom = 13,
   children,
 }) => {
   const { baseLayer, setBaseLayer } = useMapBaseLayer()
@@ -41,6 +43,7 @@ const Map: React.FC<MapProps> = ({
             format: 'image/png',
           }}
           url="https://www.gmrt.org/services/mapserver/wms_merc?"
+          maxNativeZoom={maxNativeZoom}
           eventHandlers={{
             add: addBaseLayerHandler('GMRT'),
           }}
@@ -58,6 +61,8 @@ const Map: React.FC<MapProps> = ({
       style={style}
       minZoom={minZoom}
       maxZoom={maxZoom}
+      // @ts-ignore
+      maxNativeZoom={maxNativeZoom}
     >
       <LayersControl position="topright">
         <LayersControl.BaseLayer
@@ -67,16 +72,14 @@ const Map: React.FC<MapProps> = ({
           <TileLayer
             url="https://ibasemaps-api.arcgis.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}?//token=<ACCESS_TOKEN>process.env.REACT_APP_ESRI_API_KEY</ACCESS_TOKEN>"
             attribution='&copy; <a href="https://developers.arcgis.com/">ArcGIS</a>'
+            maxNativeZoom={maxNativeZoom}
             eventHandlers={{
               add: addBaseLayerHandler('ESRI Oceans/Labels'),
             }}
           />
         </LayersControl.BaseLayer>
         {gmrtLayer}
-        <LayersControl.BaseLayer
-          name="OpenStreetmaps"
-          //checked={baseLayer === 'OpenStreetmaps'}
-        >
+        <LayersControl.BaseLayer name="OpenStreetmaps">
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
