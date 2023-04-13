e546cd60a5dd88413944d950aea33bf190b259c4
Jim Jeffers <jim@sumocreations.com>
2023-04-13 13:09:37 -0700

    Refactored `makeCommand` method to exclude sched syntax.

--- apps/lrauv-dash2/components/MissionModal.tsx
+++ apps/lrauv-dash2/components/MissionModal.tsx
@@ -338,20 +338,22 @@ const MissionModal: React.FC<MissionModalProps> = ({
     notes,
     preview,
   }) => {
-    const missionCommand = makeMissionCommand({
+    const { commandText, schedDate, previewSbd } = makeMissionCommand({
       mission: selectedMissionId as string,
       parameterOverrides,
       scheduleMethod: scheduleMethod as ScheduleOption,
       specifiedTime: specifiedTime ?? undefined,
     })
-    setCommandText(missionCommand)
+    console.log('previewSbd', previewSbd)
+    console.log('commandText', commandText)
+    setCommandText(previewSbd)
     if (!preview) {
       createCommand({
         vehicle: confirmedVehicle?.toLowerCase() ?? '',
         path: selectedMission as string,
         commandNote: notes ?? '',
         runCommand: 'y',
-        schedDate: specifiedTime ?? 'asap',
+        schedDate,
         destinationAddress: alternateAddress ?? undefined,
         commandText: commandText ?? '',
       })
