ee8bdfeb5028ade191c6f4ee081a26bc98ef975e
Jim Jeffers <jim@sumocreations.com>
2023-06-02 14:14:39 -0700

    Removed unused vars / code.

--- packages/react-ui/src/Map/MouseCoordinates.tsx
+++ packages/react-ui/src/Map/MouseCoordinates.tsx
@@ -2,19 +2,6 @@
 import React from 'react'
 import { useMapEvents } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import { createControlComponent } from '@react-leaflet/core'
-import { Control } from 'leaflet'
-import L from 'leaflet'
-
-// Classes used by Leaflet to position controls
-const POSITION_CLASSES = {
-  bottomleft: 'leaflet-bottom leaflet-left',
-  bottomright: 'leaflet-bottom leaflet-right',
-  topleft: 'leaflet-top leaflet-left',
-  topright: 'leaflet-top leaflet-right',
-}
-
-const BOUNDS_STYLE = { weight: 1 }
 
 let divStyle = {
   color: 'darkblue',
@@ -28,8 +15,6 @@ let divStyle = {
     '0 5px 5px -3px rgba(0, 0, 0, 0.2), 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12)',
   cursor: 'pointer',
 }
-
-export type MouseCoordinatesProps = Control.MouseCoordinatesOptions
 
 function formatLatitude(latitude) {
   latitude = `${latitude.toFixed(5)}`
@@ -75,33 +60,15 @@ function MouseCoordinates(props) {
     mousemove(event) {
       setMousePoint(event.latlng)
     },
-    mouseout() {
-      // setMousePoint(null)
-    },
   })
 
   if (formattedCoordinates.length === 0) return null
 
   return (
-    <div
-      className="leaflet-control leaflet-bar"
-      style={divStyle}
-      pathOptions={BOUNDS_STYLE}
-    >
+    <div className="leaflet-control leaflet-bar" style={divStyle}>
       {formattedCoordinates}
     </div>
   )
 }
-
-// export type MouseCoordinatesCtlProps = Control.MouseCoordinatesCtlOptions
-
-// export const MouseCoordinatesCtl = createControlComponent
-//     <
-//     Control.MouseCoordinatesCtl,
-//     MouseCoordinatesCtlProps
-//     >
-//    (function createMouseCoordinatesCtl(props) {
-//        return new Control.MouseCoordinatesCtl(props)
-//    })
 
 export default MouseCoordinates
