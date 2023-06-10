be1e15851d8063ff3dd1b3d084d819625654a7be
Jim Jeffers <jim@sumocreations.com>
2023-06-09 17:29:30 -0700

    Deployment map makes requests to google API for elevation when needed.

--- apps/lrauv-dash2/components/DeploymentMap.tsx
+++ apps/lrauv-dash2/components/DeploymentMap.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic'
 import { useCallback } from 'react'
 import { useManagedWaypoints } from '@mbari/react-ui'
+import { useSiteConfig } from 'api-client'
 
 // This is a tricky workaround to prevent leaflet from crashing next.js
 // SSR. If we don't do this, the leaflet map will be loaded server side
@@ -29,6 +30,35 @@ interface DeploymentMapProps {
   endTime?: number | null
 }
 
+// Define a type for the LatLng input to the function
+interface LatLng {
+  latitude: number
+  longitude: number
+}
+
+// Define types for the API response
+interface ElevationResponse {
+  results: Array<{ elevation: number }>
+  status: string
+}
+
+async function getElevation(
+  { latitude, longitude }: LatLng,
+  apiKey: string
+): Promise<number | null> {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/elevation/json?locations=${latitude}%2C${longitude}&key=${apiKey}`
+  )
+  const data: ElevationResponse = await response.json()
+
+  if (data.status !== 'OK') {
+    console.error('Failed to get elevation:', data.status)
+    return null
+  }
+
+  return data.results[0].elevation
+}
+
 const DeploymentMap: React.FC<DeploymentMapProps> = ({
   vehicleName,
   indicatorTime,
@@ -55,8 +85,29 @@ const DeploymentMap: React.FC<DeploymentMapProps> = ({
     (wp) => ![wp.lat?.toLowerCase(), wp.lon?.toLowerCase()].includes('nan')
   )
 
+  const { data } = useSiteConfig({})
+  const googleApiKey = data?.appConfig.googleApiKey
+
+  const handleDepthRequest = useCallback(
+    async (lat: number, lng: number) => {
+      if (!googleApiKey) {
+        return 0
+      }
+      const elevation = await getElevation(
+        { latitude: lat, longitude: lng },
+        googleApiKey
+      )
+      return elevation ?? 0
+    },
+    [googleApiKey]
+  )
+
   return (
-    <Map className="h-full w-full" maxZoom={17}>
+    <Map
+      className="h-full w-full"
+      maxZoom={17}
+      onRequestDepth={handleDepthRequest}
+    >
       {plottedWaypoints?.length ? (
         <>
           {plottedWaypoints.map((m, i) => {
