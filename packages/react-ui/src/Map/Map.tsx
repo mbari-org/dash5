7d960488cd8ef7e866515e7dcb73fae8b4dcf219
Karen A. Salamy <salamy@mbari.org>
2024-07-01 16:22:59 -0700

    Update Map.tsx

--- packages/react-ui/src/Map/Map.tsx
+++ packages/react-ui/src/Map/Map.tsx
@@ -4,21 +4,34 @@ import {
   WMSTileLayer,
   LayersControl,
   ScaleControl,
-  useMap,
+  useMapEvents,
 } from 'react-leaflet'
 import Control from 'react-leaflet-custom-control'
 import 'leaflet/dist/leaflet.css'
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import 'leaflet-measure/dist/leaflet-measure.css'
+import '../css/geoManControl.css'
+import 'react-tooltip/dist/react-tooltip.css'
+import React, { useCallback, useMemo, useState } from 'react'
+import Symbology, { SymbologyProps } from './Symbology'
 import ReactLeafletGoogleLayer from 'react-leaflet-google-layer'
 import MouseCoordinates, { MouseCoordinatesProps } from './MouseCoordinates'
 import { useMapBaseLayer, BaseLayerOption } from './useMapBaseLayer'
 import 'leaflet-mouse-position'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faArrowsToDot,
+  faCircleCheck,
   faRulerCombined,
 } from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons'
 import { Measurement } from './Measurement'
+import { Tooltip } from 'react-tooltip'
+import { GeomanControl } from './GeomanControl'
+import MovingDot from './MovingDot'
+import { AreaComponent, PathComponent, MeasurementProps } from './Measurement'
+
+const regex = /\B(?=(\d{3})+(?!\d))/g
+let mapCoord: String
+let dmsCoord: String
 
 export interface MapProps {
   className?: string
@@ -30,23 +43,12 @@ export interface MapProps {
   maxNativeZoom?: number
   scrollWheelZoom?: boolean
   onRequestDepth?: MouseCoordinatesProps['onRequestDepth']
-  onRequestCoordinate?: () => void
+  dmsCoord?: string
+  mapCoord?: string
   children?: React.ReactNode
 }
 
-export type MeasureMode = 'closed' | 'open' | 'measuring'
-
-// Special component to center the map to a specific location
-// when we change the center prop on the parent map.
-const CenterView: React.FC<{ coords: [number, number] }> = ({ coords }) => {
-  const map = useMap()
-
-  useEffect(() => {
-    map.setView(coords, map.getZoom())
-  }, [coords, map])
-
-  return null
-}
+export type MeasureMode = 'open' | 'measuring' | 'closed' | 'cancelled'
 
 const Map: React.FC<MapProps> = ({
   className,
@@ -58,7 +60,6 @@ const Map: React.FC<MapProps> = ({
   maxNativeZoom = 13,
   children,
   onRequestDepth,
-  onRequestCoordinate,
 }) => {
   const { baseLayer, setBaseLayer } = useMapBaseLayer()
   const addBaseLayerHandler = useCallback(
@@ -67,9 +68,135 @@ const Map: React.FC<MapProps> = ({
     },
     [setBaseLayer]
   )
+  // Create measurements
   const [measurements, setMeasurements] = useState<
-    { id: string; editing?: boolean }[]
+    {
+      id: string
+      editing?: boolean
+    }[]
   >([])
+
+  const [count, setCount] = useState(0)
+  const [isHovering, setIsHovering] = useState(false)
+  const symbols = new Symbology({})
+
+  const measStyle = {
+    color: '#00008b',
+    fontFamily: 'Helvetica Neue, Arial, Helvetica, sans- serif',
+    fontSize: '12px',
+  }
+
+  // Function to convert coordinate in deg to DMS
+  function ConvertDEGToDMS(deg: number, dir: boolean) {
+    var absolute = Math.abs(deg)
+    var degrees = Math.floor(absolute)
+    var minutesNotTruncated = (absolute - degrees) * 60
+    var minutes = Math.floor(minutesNotTruncated)
+    var seconds = ((minutesNotTruncated - minutes) * 60).toFixed(2)
+    if (dir) {
+      var direction = deg >= 0 ? 'N' : 'S'
+    } else {
+      var direction = deg >= 0 ? 'E' : 'W'
+    }
+    return degrees + '° ' + minutes + "' " + seconds + '" ' + direction
+  }
+
+  ///////////////////////////////////////////////////////////////
+  // Clicking on and determining map coordinates and formatting
+  //////////////////////////////////////////////////////////////
+  const MeasureEvents = () => {
+    useMapEvents({
+      click(e) {
+        let dir = true
+        mapCoord = e.latlng.lat.toFixed(6) + '  /  ' + e.latlng.lng.toFixed(6)
+        let latDMS = ConvertDEGToDMS(e.latlng.lat, dir)
+        dir = false
+        let lngDMS = ConvertDEGToDMS(e.latlng.lng, dir)
+        dmsCoord = latDMS + ' / ' + lngDMS
+        setCount(count + 1)
+      },
+    })
+    return (
+      <div hidden>
+        {dmsCoord}
+        {mapCoord}
+      </div>
+    )
+  }
+
+  let element = <div></div>
+  if (count == 0) {
+    element = (
+      <>
+        Continue clicking on the map to measure distance or area
+        <br />
+        <br />
+      </>
+    )
+  } else if (count == 1) {
+    element = (
+      <>
+        Last Point
+        <br />
+        <div style={measStyle}>
+          {dmsCoord}
+          <br />
+          {mapCoord}
+        </div>
+        <hr className="hr-round"></hr>
+        <br />
+      </>
+    )
+  } else if (count == 2) {
+    element = (
+      <>
+        Last Point
+        <br />
+        <div style={measStyle}>
+          {dmsCoord}
+          <br />
+          {mapCoord}
+        </div>
+        <hr className="hr-round"></hr>
+        <br />
+        Path Distance
+        <br />
+        <div style={measStyle}>
+          <PathComponent />
+        </div>
+        <hr className="hr-round"></hr>
+        <br />
+      </>
+    )
+  } else if (count >= 3) {
+    element = (
+      <>
+        Last Point
+        <br />
+        <div style={measStyle}>
+          {dmsCoord}
+          <br />
+          {mapCoord}
+        </div>
+        <hr className="hr-round"></hr>
+        <br />
+        Path Distance
+        <br />
+        <div style={measStyle}>
+          <PathComponent />
+        </div>
+        <hr className="hr-round"></hr>
+        <br />
+        Area
+        <br />
+        <div style={measStyle}>
+          <AreaComponent />
+        </div>
+        <hr className="hr-round"></hr>
+        <br />
+      </>
+    )
+  }
 
   const gmrtLayer = useMemo(
     () => (
@@ -91,31 +218,64 @@ const Map: React.FC<MapProps> = ({
     ),
     [addBaseLayerHandler, maxNativeZoom, minZoom, maxZoom]
   )
+
+  ////////////////////////////////////////
+  // Create measureMode and setMeasureMode
   const [measureMode, setMeasureMode] = useState<MeasureMode>('closed')
+  // Create showDot and setShowDot
+  const [visibleDot, setVisibleDot] = useState('hidden')
+  // Set the default cursor
+  const [cursor, setCursor] = useState('default')
+  const [tooltipOpen, setTooltipOpen] = useState(false)
+
+  ///////////////////////////////////////////
+  //  changeMeasureMode
   const changeMeasureMode = (mode: MeasureMode) => (e: React.MouseEvent) => {
     e.preventDefault()
     e.stopPropagation()
+
+    // console.log('MeasureMode is set to: ' + mode)
+    if (mode === 'open') {
+      setCount(0)
+    }
     if (mode === 'measuring') {
+      setCount(0)
       setMeasurements((prev) => [
         ...prev,
+        // { id: measurements.length.toString(), editing: true },
         { id: Date.now().toLocaleString(), editing: true },
       ])
     }
     if (mode === 'closed') {
-      setMeasurements((prev) => prev.map((p) => ({ ...p, editing: false })))
+      setCount(0)
+      setMeasurements((prev) =>
+        prev.map((p) => ({
+          ...p,
+          editing: false,
+        }))
+      )
+      // setMeasureMode('cancelled')
     }
+    if (mode === 'cancelled') {
+      setCount(0)
+      setMeasurements((prev) => [])
+    }
+    // setCount(0)
     setMeasureMode(mode)
   }
 
-  const handleRequestCoordinate = (e: React.MouseEvent) => {
-    e.preventDefault()
-    e.stopPropagation()
-    onRequestCoordinate?.()
+  const HandleMouseOver = () => {
+    setVisibleDot('hidden')
   }
 
-  const removeMeasurment = (id: string) => () => {
-    setMeasurements((prev) => prev.filter((m) => m.id !== id))
-  }
+  // removeMeasurement
+  const removeMeasurement = useCallback(
+    (id: string) => () => {
+      setMeasurements((prev) => prev.filter((m) => m.id !== id))
+      console.log(measurements, id)
+    },
+    [measurements, setMeasurements]
+  )
 
   return (
     <MapContainer
@@ -182,67 +342,114 @@ const Map: React.FC<MapProps> = ({
           />
         </LayersControl.BaseLayer>
       </LayersControl>
-      {children}
-      {measurements.map((m) => (
-        <Measurement
-          key={m.id}
-          editing={m.editing}
-          onDelete={removeMeasurment(m.id)}
-        />
-      ))}
+      <Control prepend position="topright">
+        <MouseCoordinates onRequestDepth={onRequestDepth} />
+      </Control>
       <div className={'leaflet-control'}>{children}</div>
       <Control position="topright">
-        {measureMode === 'measuring' ? (
-          <div
-            className="rounded bg-white p-2 text-stone-500"
-            style={{
-              border: '2px solid rgba(0,0,0,0.2)',
-              backgroundClip: 'padding-box',
-              maxWidth: 160,
-            }}
-          >
-            <p className="pb-2">
-              Click any where on the map to create a measurement.
-            </p>
-            <button
-              onClick={changeMeasureMode('closed')}
-              className="w-full rounded border bg-primary-600 p-1 text-white"
-            >
-              Done
-            </button>
-          </div>
-        ) : null}
+        {children}
+        {measurements.map((m) => (
+          <React.Fragment key={m.id}>
+            <Measurement
+              editing={m.editing}
+              onDelete={removeMeasurement(m.id)}
+            />
+            <MovingDot editing={m.editing} />
+          </React.Fragment>
+        ))}
         {measureMode === 'open' ? (
           <div
-            className="rounded bg-white p-2 text-stone-500"
+            className="leaflet-grab rounded bg-white p-2 text-stone-500"
+            onDragStart={() => setCursor('pointer')}
             style={{
               border: '2px solid rgba(0,0,0,0.2)',
               backgroundClip: 'padding-box',
-              maxWidth: 160,
+              width: 250,
+              maxWidth: 250,
             }}
           >
-            <p className="pb-2">
-              Start a new measurement to measure distance / area between points.
-            </p>
-            <ul className="grid grid-cols-2 gap-2">
-              <button
+            <p className="measure-info" cursor-pointer>
+              <a
+                id="createMeas"
+                className="mousechange:hover leaflet-grab w-full bg-white p-1 text-primary-600"
                 onClick={changeMeasureMode('measuring')}
-                className="rounded border bg-primary-600 p-1 text-white"
               >
-                New
-              </button>
+                <FontAwesomeIcon
+                  icon={faCircleCheck}
+                  size="lg"
+                  id="circleCheck"
+                />
+                {'    '} Create A New Measurement {'    '}
+              </a>
               <button
-                onClick={changeMeasureMode('closed')}
-                className="rounded border border-primary-600 p-1 text-primary-600"
+                onClick={changeMeasureMode('cancelled')}
+                className="mousechange:hover cursor-pointer:onHover p-1"
               >
-                Cancel
+                <FontAwesomeIcon
+                  icon={faCircleXmark}
+                  size="lg"
+                  id="xMark"
+                  style={{ marginLeft: '.5rem' }}
+                />
+              </button>
+            </p>
+          </div>
+        ) : null}
+        {measureMode === 'measuring' ? (
+          <div
+            className="cursor-pointer:onHover rounded bg-white p-2 text-stone-500"
+            style={{
+              border: '2px solid rgba(0,0,0,0.2)',
+              backgroundClip: 'padding-box',
+              width: 250,
+              maxWidth: 250,
+            }}
+          >
+            <p className="measure-info cursor-pointer:onHover">
+              <br />
+              <h6>
+                <span className="font-bold text-blue-800">
+                  Measure Distances and Areas
+                </span>
+              </h6>
+              <br />
+              <hr className="hr-round"></hr>
+              <br />
+              {element}
+            </p>
+            <ul className="mousechange leaflet-grab grid">
+              <button
+                id="finishMeas"
+                className="leaflet-grab w-full rounded border bg-blue-600 p-1 text-white hover:bg-blue-800"
+                onMouseOver={HandleMouseOver}
+                style={{
+                  position: 'relative',
+                  zIndex: isHovering ? 900 : 10,
+                }}
+                onClick={changeMeasureMode('closed')}
+              >
+                <FontAwesomeIcon icon={faCircleCheck} /> Finish Measurement
+              </button>
+              <br />
+              <button
+                id="cancelMeas"
+                className="leaflet-grab w-full rounded border bg-white p-1 text-primary-600 hover:bg-gray-100"
+                onMouseOver={HandleMouseOver}
+                style={{
+                  position: 'relative',
+                  zIndex: isHovering ? 900 : 10,
+                }}
+                onClick={changeMeasureMode('cancelled')}
+              >
+                <FontAwesomeIcon icon={faCircleXmark} /> Cancel
               </button>
             </ul>
           </div>
         ) : null}
         {measureMode === 'closed' ? (
           <button
-            className="rounded bg-white p-2 text-stone-500"
+            className="mousechange rounded bg-white p-2 text-stone-500"
+            onDragEnd={() => setCursor('default')}
             style={{
               border: '2px solid rgba(0,0,0,0.2)',
               backgroundClip: 'padding-box',
@@ -250,29 +457,64 @@ const Map: React.FC<MapProps> = ({
             }}
             onClick={changeMeasureMode('open')}
           >
-            <FontAwesomeIcon icon={faRulerCombined} size="2xl" />
+            <a
+              data-tooltip-id="measure-tooltip"
+              data-tooltip-content="Measure Distances and Areas"
+              data-tooltip-place="bottom-end"
+            >
+              <FontAwesomeIcon
+                icon={faRulerCombined}
+                size="2xl"
+                color="#046B04"
+              />
+            </a>
+            <Tooltip
+              id="measure-tooltip"
+              style={{
+                backgroundColor: 'rgb(255,255,255)',
+                color: 'blue',
+                fontWeight: 'bold',
+                fontSize: '12px',
+              }}
+            ></Tooltip>
           </button>
         ) : null}
-      </Control>
-      <Control prepend position="topright">
-        <MouseCoordinates onRequestDepth={onRequestDepth} />
-      </Control>
-      {onRequestCoordinate ? (
-        <Control position="topright">
+        {measureMode === 'cancelled' ? (
           <button
-            className="rounded bg-white p-2 text-stone-500"
+            className="mousechange rounded bg-white p-2 text-stone-500"
+            onDragEnd={() => setCursor('default')}
             style={{
               border: '2px solid rgba(0,0,0,0.2)',
               backgroundClip: 'padding-box',
               width: 48,
             }}
-            onClick={handleRequestCoordinate}
+            // onClick={changeMeasureMode('open')}
           >
-            <FontAwesomeIcon icon={faArrowsToDot} size="2xl" />
+            <a
+              data-tooltip-id="measure-tooltip"
+              data-tooltip-content="Measure Distances and Areas"
+              data-tooltip-place="bottom-end"
+            >
+              <FontAwesomeIcon
+                icon={faRulerCombined}
+                size="2xl"
+                color="#046B04"
+              />
+            </a>
+            <Tooltip
+              id="measure-tooltip"
+              style={{
+                backgroundColor: 'rgb(255,255,255)',
+                color: 'blue',
+                fontWeight: 'bold',
+                fontSize: '12px',
+              }}
+            ></Tooltip>
           </button>
-        </Control>
-      ) : null}
-      <CenterView coords={center} />
+        ) : null}
+      </Control>
+      <MeasureEvents />
+      <GeomanControl position="topleft" drawCircle={true} oneBlock={true} />
     </MapContainer>
   )
 }
