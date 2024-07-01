752b1b8afa3f5c92606f032fe5f913f607bc2bf4
Karen A. Salamy <salamy@mbari.org>
2024-07-01 16:25:01 -0700

    Update Map.stories.tsx

--- packages/react-ui/src/Map/Map.stories.tsx
+++ packages/react-ui/src/Map/Map.stories.tsx
@@ -1,25 +1,20 @@
 import { Story, Meta } from '@storybook/react'
 import Map, { MapProps } from './Map'
-import { useCallback, useState } from 'react'
+// import { useCallback, useState } from 'react'
 
 export default {
   title: 'Maps/Map',
 } as Meta
 
 const Template: Story<MapProps> = (args) => {
-  const [center, setCenter] = useState(args.center)
-  const onRequestCoordinate = useCallback(() => {
-    setCenter([37.7749 + Math.random() * 4, -122.4194 + Math.random() * 4])
-  }, [setCenter])
+  //   const [center, setCenter] = useState(args.center)
+  //   const onRequestCoordinate = useCallback(() => {
+  //     setCenter([37.7749 + Math.random() * 4, -122.4194 + Math.random() * 4])
+  //   }, [setCenter])
 
   return (
     <div className="rounded border p-4">
-      <Map
-        {...args}
-        className="h-96 w-full"
-        onRequestCoordinate={onRequestCoordinate}
-        center={center}
-      />
+      <Map {...args} className="h-96 w-full" />
     </div>
   )
 }
@@ -32,5 +27,5 @@ const args: MapProps = {
   },
 }
 
-export const Primary = Template.bind({})
-Primary.args = args
+// export const Primary = Template.bind({})
+// Primary.args = args
