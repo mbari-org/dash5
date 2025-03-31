dfa0f34b4386d434bf410075524ab044ca7783bc
Zack Lyon <zack.j.lyon@gmail.com>
2025-03-31 12:19:40 -0700

    Optionally request unique recent runs based on mission name and overrides

--- packages/api-client/src/react-query/Command/useRecentRuns.ts
+++ packages/api-client/src/react-query/Command/useRecentRuns.ts
@@ -1,8 +1,16 @@
 import { useEvents } from '../Event/useEvents'
 import { SupportedQueryOptions } from '../types'
+import { extractOverrides } from '../../axios/Util/extractOverrides'
+import { generateMissionKey } from '../../axios/Util/generateMissionKey'
 
 export const useRecentRuns = (
-  params: { vehicles: string[]; from: number; to?: number; limit?: number },
+  params: {
+    vehicles: string[]
+    from: number
+    to?: number
+    limit?: number
+    unique?: boolean
+  },
   options?: SupportedQueryOptions
 ) => {
   const query = useEvents(
@@ -14,12 +22,32 @@ export const useRecentRuns = (
     options
   )
 
-  const data = query.data?.map((event) => {
+  const formattedData = query.data?.map((event) => {
+    const mission = event.data?.match(/[a-zA-Z_/]+(\.xml|\.tl)/)?.[0] ?? ''
+    const { waypointOverrides, parameterOverrides } = extractOverrides(
+      event.data ?? ''
+    )
+    const missionKey = generateMissionKey({
+      missionName: mission,
+      waypointOverrides,
+      parameterOverrides,
+    })
     return {
       ...event,
-      mission: event.data?.match(/[a-zA-Z_/]+(\.xml|\.tl)/)?.[0] ?? '',
+      mission,
+      waypointOverrides,
+      parameterOverrides,
+      missionKey,
     }
   })
+
+  // Provide unique missions (unique mission name and waypoint and parameter overrides) if requested
+  const data = params.unique
+    ? formattedData?.filter(
+        (event, index, self) =>
+          self.findIndex((e) => e.missionKey === event.missionKey) === index
+      )
+    : formattedData
 
   return { ...query, data }
 }
