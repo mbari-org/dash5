e4bbadcd4329629be6169d4de211584e9f14ac53
Zack Lyon <zack.j.lyon@gmail.com>
2025-04-03 16:46:30 -0700

    Create hook to get PIC and OnCall operators for inactive vehicles

---
+++ packages/api-client/src/react-query/User/useVehiclePicAndOnCall.ts
@@ -0,0 +1,66 @@
+import { useMemo } from 'react'
+import { extractPicAndOnCallFromNotes } from '../../axios/Util/extractPicAndOnCallFromNotes'
+import { useEvents } from '../Event/useEvents'
+import { DateTime } from 'luxon'
+import { getAdjustedUnixTime } from '@mbari/utils'
+
+const TWO_YEARS_AGO = getAdjustedUnixTime({
+  unixTime: DateTime.now().toMillis(),
+  offsetYears: -2,
+})
+
+export interface VehiclePicAndOnCallUser {
+  user: string
+  unixTime: number
+}
+
+export interface VehiclePicAndOnCallResponse {
+  pic: VehiclePicAndOnCallUser | null
+  onCall: VehiclePicAndOnCallUser | null
+  isLoading: boolean
+}
+
+export interface UseVehiclePicAndOnCallParams {
+  vehicleName: string
+  from?: number // milliseconds since epoch
+  to?: number // milliseconds since epoch
+  enabled?: boolean
+}
+
+/**
+ * Hook to get the PIC and OnCall users for a vehicle from note events (current endpoint for PIC and OnCall only retrieves roles from active deployments)
+ * @param params Parameters including vehicleName and time range
+ * @returns The PIC and OnCall users found in the notes, with loading state
+ */
+export const useVehiclePicAndOnCall = ({
+  vehicleName,
+  from,
+  to,
+  enabled = true,
+}: UseVehiclePicAndOnCallParams): VehiclePicAndOnCallResponse => {
+  const { data: noteEvents, isLoading } = useEvents(
+    {
+      vehicles: [vehicleName],
+      from: from ?? TWO_YEARS_AGO,
+      eventTypes: ['note'],
+      to,
+      limit: 3000,
+    },
+    {
+      enabled,
+    }
+  )
+
+  const picAndCall = useMemo(() => {
+    if (!noteEvents?.length) {
+      return { pic: null, onCall: null }
+    }
+
+    return extractPicAndOnCallFromNotes(noteEvents)
+  }, [noteEvents])
+
+  return {
+    ...picAndCall,
+    isLoading,
+  }
+}
