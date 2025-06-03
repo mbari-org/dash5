09796ed7e674c9b1c761d72fc562def831b6cdde
Zack Lyon <zack.j.lyon@gmail.com>
2025-06-02 18:57:23 -0700

    Fix flickering during initial load

--- packages/api-client/src/react-query/Event/useCommsEvents.tsx
+++ packages/api-client/src/react-query/Event/useCommsEvents.tsx
@@ -55,36 +55,63 @@ export const useCommsEvents = ({
     return data.pages.flat()
   }, [data?.pages])
 
-  const commands = useMemo(() => {
-    const commands: GetEventsResponse[] = []
-    flatData.forEach((e) => {
-      if (e.eventType === 'command' || e.eventType === 'run') {
-        commands.push(e)
-      }
-    })
-    return commands
-  }, [flatData])
+  const { commands, sbdSendMap, sbdReceiptMap, sbdReceiveMap, timeoutMap } =
+    useMemo(() => {
+      const commands: GetEventsResponse[] = []
+      const sbdSendMap = new Map<string, GetEventsResponse>()
+      const sbdReceiptMap = new Map<string, GetEventsResponse>()
+      const sbdReceiveMap = new Map<number, GetEventsResponse>()
+      const timeoutMap = new Map<string, GetEventsResponse>()
+
+      flatData.forEach((e) => {
+        switch (e.eventType) {
+          case 'command':
+          case 'run':
+            commands.push(e)
+            break
+          case 'sbdSend':
+            if (e.refId !== undefined) sbdSendMap.set(String(e.refId), e)
+            break
+          case 'sbdReceipt':
+            const id = e.name?.match(digitsForIdRegEx)?.[0]
+            if (id) sbdReceiptMap.set(id, e)
+            break
+          case 'sbdReceive':
+            if (e.mtmsn) sbdReceiveMap.set(e.mtmsn, e)
+            break
+          case 'note':
+            const timeoutMatch = e.note?.match(timeoutExpiredRegEx)
+            if (timeoutMatch && timeoutMatch[1]) {
+              timeoutMap.set(timeoutMatch[1], e)
+            }
+            break
+        }
+      })
+
+      return { commands, sbdSendMap, sbdReceiptMap, sbdReceiveMap, timeoutMap }
+    }, [flatData])
 
   const [isFetchingMore, setIsFetchingMore] = useState(false)
   const [targetCommandCount, setTargetCommandCount] = useState(0)
 
-  // If initial fetch does not have enough commands/missions, fetch more (this avoids sending back an empty array of updated commands)
+  // If initial fetch does not have enough commands/missions, fetch more (this avoids sending back an empty array of updated commands and ui flickering during the initial load)
+  const fetchingInitialCommands =
+    commands.length < minCommandEvents && hasNextPage
+
   useEffect(() => {
-    if (!isLoading && !isFetchingNextPage && !isFetchingMore && hasNextPage) {
-      if (commands.length < minCommandEvents) {
-        fetchNextPage()
-      }
+    if (fetchingInitialCommands && !isLoading && !isFetchingNextPage) {
+      fetchNextPage()
+      return
     }
   }, [
-    commands.length,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingMore,
-    isFetchingNextPage,
+    fetchingInitialCommands,
     isLoading,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
   ])
 
-  // If we're manually fetching more commands, keep fetching until we have more commands (this makes sure that additional commands are fetched and not just other types of events)
+  // If we're manually fetching more commands, keep fetching until we have more commands (this makes sure that additional commands are fetched not just sbd/note events)
   useEffect(() => {
     if (isFetchingMore) {
       if (
@@ -130,50 +157,6 @@ export const useCommsEvents = ({
     isFetchingMore,
   ])
 
-  const sbdSendMap = useMemo(() => {
-    const map = new Map<string, GetEventsResponse>()
-    flatData.forEach((e) => {
-      if (e.eventType === 'sbdSend' && e.refId !== undefined) {
-        map.set(String(e.refId), e)
-      }
-    })
-    return map
-  }, [flatData])
-
-  const sbdReceiptMap = useMemo(() => {
-    const map = new Map<string, GetEventsResponse>()
-    flatData.forEach((e) => {
-      if (e.eventType === 'sbdReceipt') {
-        const id = e.name?.match(digitsForIdRegEx)?.[0]
-        if (id) map.set(id, e)
-      }
-    })
-    return map
-  }, [flatData])
-
-  const sbdReceiveMap = useMemo(() => {
-    const map = new Map<number, GetEventsResponse>()
-    flatData.forEach((e) => {
-      if (e.eventType === 'sbdReceive' && e.mtmsn) {
-        map.set(e.mtmsn, e)
-      }
-    })
-    return map
-  }, [flatData])
-
-  const timeoutMap = useMemo(() => {
-    const map = new Map<string, GetEventsResponse>()
-    flatData.forEach((e) => {
-      if (e.eventType === 'note') {
-        const timeoutMatch = e.note?.match(timeoutExpiredRegEx)
-        if (timeoutMatch && timeoutMatch[1]) {
-          map.set(timeoutMatch[1], e)
-        }
-      }
-    })
-    return map
-  }, [flatData])
-
   const updatedCommands = useMemo((): CommsEvent[] => {
     return commands.map((command) => {
       return determineCommandStatus(
@@ -186,10 +169,12 @@ export const useCommsEvents = ({
     })
   }, [commands, sbdSendMap, sbdReceiptMap, sbdReceiveMap, timeoutMap])
 
+  const fetching = isFetching || isFetchingMore || isFetchingNextPage
+
   return {
     data: updatedCommands,
-    isLoading,
-    isFetching: isFetching || isFetchingMore || isFetchingNextPage,
+    isLoading: fetchingInitialCommands ? fetchingInitialCommands : isLoading,
+    isFetching: fetching,
     fetchNextPage,
     hasNextPage,
     refetch,
