ede20d3ac393bdb5ca876adb44b2bfc99f713eaa
Zack Lyon <zack.j.lyon@gmail.com>
2025-07-14 12:32:04 -0700

    Display human readable boolean values

--- packages/utils/src/makeValueUnitString.ts
+++ packages/utils/src/makeValueUnitString.ts
@@ -1,5 +1,6 @@
 // Creates a combined string out of a value and a unit
 // Works for speed, velocity, etc type units that are separated by underscores (ie meter_per_second)
+// Displays boolean values as true/false instead of 1/0
 
 export const makeValueUnitString = (value: string, unit?: string) => {
   if (!unit) return value
@@ -10,6 +11,11 @@ export const makeValueUnitString = (value: string, unit?: string) => {
       .join('_')
 
     return `${value} ${multiwordUnit}`
+  }
+  if (unit === 'bool') {
+    const boolValue =
+      value.toLowerCase() === 'true' || value === '1' ? 'true' : 'false'
+    return `${boolValue} ${unit}`
   }
   const valueAndUnit = `${value} ${unit}`
 
