d4c2a9f4afcb8b90d016068a64dc1d9a50cacbc8
Zack Lyon <zack.j.lyon@gmail.com>
2025-09-11 15:51:37 -0700

    Make unit helper function case sensitive

--- packages/utils/src/getUnitFromAbbreviation.ts
+++ packages/utils/src/getUnitFromAbbreviation.ts
@@ -4,7 +4,5 @@ export const getUnitFromAbbreviation = (
 ) => {
   if (!abbreviation || !unitsData) return undefined
   if (abbreviation.toLowerCase() === 'degree') return 'degree'
-  return unitsData?.find(
-    (u) => u.abbreviation?.toLowerCase() === (abbreviation ?? '').toLowerCase()
-  )?.name
+  return unitsData?.find((u) => u.abbreviation === (abbreviation ?? ''))?.name
 }
