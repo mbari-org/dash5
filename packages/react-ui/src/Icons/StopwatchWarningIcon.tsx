089c41358f449b7131c3377a76a4f74a02626fd6
Karen A. Salamy <salamy@mbari.org>
2026-05-06 13:39:18 -0700

    feat: clock-rotate-left + outline triangle badge to match flaticon reference

--- packages/react-ui/src/Icons/StopwatchWarningIcon.tsx
+++ packages/react-ui/src/Icons/StopwatchWarningIcon.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faExclamation,
-  faHourglassEnd,
-} from '@fortawesome/free-solid-svg-icons'
+import { faClockRotateLeft } from '@fortawesome/free-solid-svg-icons'
+import { faTriangleExclamation } from '@fortawesome/free-regular-svg-icons'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
 export interface StopwatchWarningIconProps {
@@ -23,17 +21,40 @@ export const StopwatchWarningIcon: React.FC<StopwatchWarningIconProps> = ({
       style={{ position: 'relative', display: 'inline-block', ...style }}
       aria-label="timeout warning icon"
     >
-      <FontAwesomeIcon icon={faHourglassEnd as IconProp} style={{ color }} />
-      <FontAwesomeIcon
-        icon={faExclamation as IconProp}
+      {/* Clock with circular arc — closest free FA match to the reference icon */}
+      <FontAwesomeIcon icon={faClockRotateLeft as IconProp} style={{ color }} />
+      {/*
+       * Warning triangle badge — two layers to get the white-fill + orange-stroke
+       * look from the reference: a white square punched behind, then the outline
+       * triangle on top in orange.
+       */}
+      <span
         style={{
-          color,
           position: 'absolute',
-          top: '-0.4em',
-          right: '-0.5em',
-          fontSize: '1em',
+          top: '-0.45em',
+          right: '-0.55em',
+          fontSize: '0.85em',
+          display: 'inline-flex',
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
-      />
+      >
+        {/* White fill behind the outline triangle */}
+        <span
+          style={{
+            position: 'absolute',
+            width: '0.55em',
+            height: '0.5em',
+            backgroundColor: 'white',
+            bottom: '0.1em',
+          }}
+        />
+        {/* Outline triangle (orange border, inherits white fill from behind) */}
+        <FontAwesomeIcon
+          icon={faTriangleExclamation as IconProp}
+          style={{ color, position: 'relative' }}
+        />
+      </span>
     </span>
   )
 }
