1300e491747e03dc0577c89ba19ac4337623e595
Karen A. Salamy <salamy@mbari.org>
2026-06-16 13:46:25 -0700

    test(api-client): assert createUser sends params without { data: } wrapper

--- packages/api-client/src/axios/User/createUser.test.ts
+++ packages/api-client/src/axios/User/createUser.test.ts
@@ -20,20 +20,32 @@ const mockResponse = {
   },
 }
 
+let capturedBody: Record<string, unknown> = {}
+
 const server = setupServer(
-  rest.post('/user', (_req, res, ctx) => {
+  rest.post('/user', (req, res, ctx) => {
+    capturedBody = req.body as Record<string, unknown>
     return res(ctx.status(200), ctx.json(mockResponse))
   })
 )
 
 beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
+afterEach(() => {
+  capturedBody = {}
+  server.resetHandlers()
+})
 afterAll(() => server.close())
 
 describe('createUser', () => {
   it('should return the mocked users when successful', async () => {
     const user = await createUser(params)
     expect(user).toEqual(mockResponse.result)
+  })
+
+  it('should send params directly in the POST body without a { data: } wrapper', async () => {
+    await createUser(params)
+    expect(capturedBody).toEqual(params)
+    expect(capturedBody).not.toHaveProperty('data')
   })
 
   it('should throw when unsuccessful', async () => {
